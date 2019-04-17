e19b3bd2b2cc6e266a1f343f8b4f189c992d87af
Mariana Filipa Costa <up201604414@fe.up.pt>
2019-04-17 16:58:41 +0100

    continuing symbol table (parameters not being read)

--- jmm/ST.java
+++ jmm/ST.java
@@ -2,13 +2,13 @@ import java.util.HashMap;
 
 public class ST extends Object {
 
-  HashMap<String, HashMap<String, STO>> functionTables;
-  Node[] AST;
+  HashMap<String, STFunction> functionTable;
+  SimpleNode AST;
 
-  public ST(Node[] AST) {
-    this.functionTables = new HashMap<String, HashMap<String, STO>>();
+  public ST(SimpleNode AST) {
+    this.functionTable = new HashMap<String, STFunction>();
     this.AST = AST;
-    //insert global scope entry in hashmap
+    analyzeAST();
   }
 
   public void analyzeAST() {
@@ -16,25 +16,56 @@ public class ST extends Object {
     //for each function create a new entry in the function table
     //maybe new method to deal with functions, called recursively
     String nodeType;
-    for(Node node : AST) {
+    Node[] children = ((SimpleNode) AST.children[0]).children;
+
+    for(Node node : children) {
       if(((SimpleNode) node).children != null) {
         nodeType = ((SimpleNode) node).getClass().getSimpleName();
-        switch(nodeType) {
-          case "ASTAssign":
-            break;
-          case "ASTAssignStatement":
-            break;
-          case "ASTArrayAssign":
-            break;
-          case "ASTArrayAssignStatement":
-            break;
-          default:
-
+        if (nodeType.equals("ASTMethodDeclaration") || nodeType.equals("ASTMainDeclaration")) {
+          analyzeMethodDeclaration(node);
         }
       }
     }
   } 
 
+  public void analyzeMethodDeclaration(Node node) {
 
+    STFunction stFunction = new STFunction();
+    String stFunctionName = null;
+    if (((SimpleNode) node).getClass().getSimpleName().equals("ASTMethodDeclaration")) {
+      stFunctionName = ((ASTMethodDeclaration) node).getName();
+      stFunction.returnDescriptor = new STO(((ASTMethodDeclaration) node).type);
+    } else {
+      stFunctionName = "main";
+    }
+    
+    //parameters
+    Node[] children = ((SimpleNode) node).children;
+    if (((SimpleNode) children[0]).getClass().getSimpleName().equals("ASTMethodArguments")) {
+      for(Node methodArg: ((SimpleNode) children[0]).children) {
+        stFunction.params.put(((ASTMethodArgument) methodArg).getIdentifier(), new STO(((ASTMethodArgument) methodArg).type));
+      }
+    }
 
+    //locals
+    for(Node childNode : children) {
+      if(!((SimpleNode) childNode).getClass().getSimpleName().equals("ASTVarDeclaration")) {
+        break;
+      } else {
+        stFunction.locals.put(((ASTVarDeclaration) childNode).getIdentifier(), new STO(((ASTVarDeclaration) childNode).type));
+      }
+    }
+
+    functionTable.put(stFunctionName, stFunction);
+  
+  }
+
+  public void dump() {
+    
+    functionTable.forEach((key, value) -> {
+      System.out.println("Function " + key + ":");
+      value.dump();
+    });
+  }
+ 
 }
